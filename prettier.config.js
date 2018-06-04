201584165258f68a32e1436247f9b270c56f5234
chenbeta <beta_king@163.com>
2018-06-04 17:08:35 +0800

    tes111

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	// tab缩进大小,默认为2
+	tabWidth: 2,
+	// 使用tab缩进，默认false
+	useTabs: true,
+	// 使用分号, 默认true
+	semi: false,
+	// 使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
+	singleQuote: true,
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	// 箭头函数参数括号 默认avoid 可选 avoid| always
+	// avoid 能省略括号的时候就省略 例如x => x
+	// always 总是有括号
+	arrowParens: 'avoid'
+}
